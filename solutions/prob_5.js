d228a40547c014abd935aeb273151b09baebeb06
Dominick Xu <nicktheturtle@NickTheTurtleMacBook-Pro.local>
2018-11-13 20:51:21 -0600

    Update solutions/prob_5.js

---
+++ solutions/prob_5.js
@@ -0,0 +1,86 @@
+console.log`University of Tennessee: Knoxville
+Middle Tennessee State University
+University of Memphis
+East Tennessee State University
+University of Tennessee: Chattanooga
+Austin Peay State University
+Tennessee Technological University
+Chattanooga State Community College
+Southwest Tennessee Community College
+Volunteer State Community College
+Vanderbilt University
+Nashville State Community College
+Tennessee State University
+Belmont University
+Northeast State Community College
+University of Tennessee: Martin
+Motlow State Community College
+Walters State Community College
+Lee University
+Bethel University
+Jackson State Community College
+Lipscomb University
+Cleveland State Community College
+Southern Adventist University
+Trevecca Nazarene University
+Union University
+Rhodes College
+University of Phoenix: Nashville
+Dyersburg State Community College
+Lincoln Memorial University
+King University
+The University of the South
+Carson-Newman University
+Tusculum College
+Christian Brothers University
+Lane College
+Freed-Hardeman University
+University of Phoenix: Memphis
+Maryville College
+Baptist College of Health Sciences
+Bryan College: Dayton
+Tennessee Wesleyan University
+LeMoyne-Owen College
+Johnson University
+Milligan College
+Fisk University
+Hiwassee College
+Aquinas College
+Nossi College of Art
+Welch College
+Watkins College of Art, Design & Film
+National College: Memphis
+University of Phoenix: Knoxville
+John A. Gupton College
+National College: Bartlett
+Visible Music College
+Fountainhead College of Technology
+National College: Madison
+National College: Bristol
+National College: Knoxville
+Huntington College of Health Sciences
+American Baptist College
+Argosy University: Nashville
+Chattanooga College
+Columbia State Community College
+Cumberland University
+Daymar Institute: Clarksville
+Daymar Institute: Murfreesboro
+Daymar Institute: Nashville
+Lincoln College of Technology: Nashville
+Martin Methodist College
+Miller-Motte Technical College: Chattanooga
+Miller-Motte Technical College: Clarksville
+National College: Nashville
+O'More College of Design
+Pellissippi State Community College
+Remington College: Memphis
+Remington College: Nashville
+Roane State Community College
+South College
+University of Phoenix: Chattanooga
+Vatterott College: Memphis
+Virginia College School of Business and Health in Chattanooga
+Virginia College School of Business and Health in Knoxville
+West Tennessee Business College
+Williamson College`
